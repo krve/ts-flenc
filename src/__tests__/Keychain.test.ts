73d53414d5a8de0c68acc33c6fcb2a4590140d53
Kristoffer Vestergaard <kristoffer@krve.io>
2019-04-04 12:27:04 +0200

    Add test

--- src/__tests__/Keychain.test.ts
+++ src/__tests__/Keychain.test.ts
@@ -1,7 +1,14 @@
-import Keychain from '../index';
+import {Keychain} from '../index';
 
-test('Keychain', () => {
+test('It can get the raw secret', () => {
     let keychain = new Keychain;
 
     expect(keychain.getRawSecret()).toBeDefined();
+});
+
+test('It can specfiy the raw secret', () => {
+    let rawSecret = crypto.getRandomValues(new Uint8Array(16));
+    let keychain = new Keychain(rawSecret);
+
+    expect(keychain.getRawSecret()).toBe(rawSecret);
 });
